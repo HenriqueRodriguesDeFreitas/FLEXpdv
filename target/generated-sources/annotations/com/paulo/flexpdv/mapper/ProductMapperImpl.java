4d3d3ba30c81e70e3c979e065e19e369b8227071
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2026-03-30 13:09:24 -0300

    fix: atualizando date no generated

--- target/generated-sources/annotations/com/paulo/flexpdv/mapper/ProductMapperImpl.java
+++ target/generated-sources/annotations/com/paulo/flexpdv/mapper/ProductMapperImpl.java
@@ -10,8 +10,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-03-18T00:56:20-0300",
-    comments = "version: 1.6.3, compiler: javac, environment: Java 25.0.2 (Oracle Corporation)"
+    date = "2026-03-29T19:57:27-0300",
+    comments = "version: 1.6.3, compiler: javac, environment: Java 21.0.10 (Oracle Corporation)"
 )
 @Component
 public class ProductMapperImpl implements ProductMapper {
