426f26990c6ec6ae9abcb171ef8ac40525f58a73
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2026-03-30 16:11:35 -0300

    feat: controller de product

---
+++ src/main/java/com/paulo/flexpdv/controller/ProductController.java
@@ -0,0 +1,51 @@
+package com.paulo.flexpdv.controller;
+
+import com.paulo.flexpdv.docs.ErrorExamples;
+import com.paulo.flexpdv.dto.request.ProductCreateRequestDto;
+import com.paulo.flexpdv.dto.response.ErrorResponseDto;
+import com.paulo.flexpdv.dto.response.ProductCreateResponseDto;
+import com.paulo.flexpdv.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("products")
+@Tag(name = "Product", description = "Operations related to product registration and modifications.")
+public class ProductController {
+
+    private final ProductService productService;
+    private final String TYPE_JSON = "application/json";
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping
+    @Operation(summary = "Create new product", description = "Used to create new product")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Product created!",
+                    content = @Content(mediaType = TYPE_JSON,
+                            schema = @Schema(implementation = ProductCreateResponseDto.class))),
+            @ApiResponse(responseCode = "409", description = "Conflict: product with this already exists",
+                    content = @Content(mediaType = TYPE_JSON,
+                            schema = @Schema(implementation = ErrorResponseDto.class),
+                            examples = {@ExampleObject(name = "product with this barcode already exists",
+                                    value = ErrorExamples.ERROR_409_BARCODE),
+                                    @ExampleObject(name = "product with this name already exists", value = ErrorExamples.ERROR_409_NAME)})),
+    })
+    public ResponseEntity<ProductCreateResponseDto> create(@RequestBody @Valid ProductCreateRequestDto request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(productService.create(request));
+    }
+}
