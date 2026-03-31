615ca9f7ac9b866fd89e5932a34dbd5deaf784fd
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2026-03-31 11:17:27 -0300

    fix: add logger

--- src/main/java/com/paulo/flexpdv/exception/GlobalExceptionHandler.java
+++ src/main/java/com/paulo/flexpdv/exception/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package com.paulo.flexpdv.exception;
 import com.paulo.flexpdv.dto.response.ErrorResponseDto;
 import com.paulo.flexpdv.exception.custom.EntityNotFoundException;
 import com.paulo.flexpdv.exception.custom.ExistingEntityConflictException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,27 +15,41 @@ import java.time.LocalDateTime;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    private ErrorResponseDto responseDto;
+    private final static Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(ExistingEntityConflictException.class)
     public ResponseEntity<ErrorResponseDto> conflictEntityExisting(
             ExistingEntityConflictException ex) {
-        responseDto = toResponse(HttpStatus.CONFLICT,
+        log.warn("Entity already exists: {}", ex.getMessage());
+        var responseDto = toResponse(HttpStatus.CONFLICT,
                 "Entity already exists.", ex);
         return ResponseEntity.status(HttpStatus.CONFLICT).body(responseDto);
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<ErrorResponseDto> notFoundException(EntityNotFoundException ex) {
-    responseDto = toResponse(HttpStatus.NOT_FOUND, "Entity not found.", ex);
-    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseDto);
+        log.warn("Entity not found: {}", ex.getMessage());
+        var responseDto = toResponse(HttpStatus.NOT_FOUND, "Entity not found.", ex);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseDto);
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorResponseDto> illegalArgument(IllegalArgumentException ex) {
-        responseDto = toResponse(HttpStatus.BAD_REQUEST,
+        log.warn("Invalid argument: {}", ex.getMessage());
+        var responseDto = toResponse(HttpStatus.BAD_REQUEST,
                 "Argument invalid", ex);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseDto);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDto> genericException(Exception ex) {
+
+        log.error("Unexpected error", ex);
+
+        var responseDto = toResponse(HttpStatus.INTERNAL_SERVER_ERROR,
+                "Unexpected error", ex);
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseDto);
     }
 
 
