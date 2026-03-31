a6009152dd0f3b03d981c27f620449520cd03058
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2026-03-31 10:21:33 -0300

    fix: criando um Exceptionhandler para entidade não encontrada.

--- src/main/java/com/paulo/flexpdv/exception/GlobalExceptionHandler.java
+++ src/main/java/com/paulo/flexpdv/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.paulo.flexpdv.exception;
 
 import com.paulo.flexpdv.dto.response.ErrorResponseDto;
+import com.paulo.flexpdv.exception.custom.EntityNotFoundException;
 import com.paulo.flexpdv.exception.custom.ExistingEntityConflictException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 public class GlobalExceptionHandler {
 
     private ErrorResponseDto responseDto;
+
     @ExceptionHandler(ExistingEntityConflictException.class)
     public ResponseEntity<ErrorResponseDto> conflictEntityExisting(
             ExistingEntityConflictException ex) {
@@ -21,8 +23,14 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(responseDto);
     }
 
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorResponseDto> notFoundException(EntityNotFoundException ex) {
+    responseDto = toResponse(HttpStatus.NOT_FOUND, "Entity not found.", ex);
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseDto);
+    }
+
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<ErrorResponseDto> illegalArgument(IllegalArgumentException ex){
+    public ResponseEntity<ErrorResponseDto> illegalArgument(IllegalArgumentException ex) {
         responseDto = toResponse(HttpStatus.BAD_REQUEST,
                 "Argument invalid", ex);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseDto);
