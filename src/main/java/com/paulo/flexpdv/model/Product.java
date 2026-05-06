a4a91cf289b192665d82660cea4c7d583b159024
PauloHenrique <devhenriquerodrigues971@gmail.com>
2026-05-06 13:28:03 -0300

    primeiro commit

---
+++ src/main/java/com/paulo/flexpdv/model/Product.java
@@ -0,0 +1,19 @@
+package com.paulo.flexpdv.model;
+
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "product")
+public class Product {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)	
+	private UUID id;
+
+}		
