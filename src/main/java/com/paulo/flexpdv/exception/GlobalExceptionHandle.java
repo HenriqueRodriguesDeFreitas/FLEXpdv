804dca2bed969b5ebe22568261a51b9456076ab7
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2026-05-19 10:36:04 -0300

    feat: add metodo de capitura de exceçãode entidade não existente.

--- src/main/java/com/paulo/flexpdv/exception/GlobalExceptionHandle.java
+++ src/main/java/com/paulo/flexpdv/exception/GlobalExceptionHandle.java
@@ -2,6 +2,7 @@ package com.paulo.flexpdv.exception;
 
 import com.paulo.flexpdv.dto.response.ErrorResponseDto;
 import com.paulo.flexpdv.exception.custom.EntityAlreadyExistsException;
+import com.paulo.flexpdv.exception.custom.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,6 +18,12 @@ public class GlobalExceptionHandle {
         ErrorResponseDto response = toResponse(HttpStatus.CONFLICT,
                 "Erro de entidade já cadastrada no banco de dados", ex);
         return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
+    }
+
+    public ResponseEntity<ErrorResponseDto> entityNotFoundException(EntityNotFoundException ex) {
+        ErrorResponseDto response = toResponse(HttpStatus.NOT_FOUND,
+                "Erro de entidade não encontrada no banco de dados", ex);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
     }
 
     private static ErrorResponseDto toResponse(HttpStatus status,
