7a7e4c91436157da4564d2306667c3e5718b11e9
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2026-05-19 10:33:42 -0300

    fix:criando metodo para verificação de existencia de categoria com nome passado e verificar seu id.

--- src/main/java/com/paulo/flexpdv/repository/CategoryRepository.java
+++ src/main/java/com/paulo/flexpdv/repository/CategoryRepository.java
@@ -7,4 +7,5 @@ import java.util.UUID;
 
 public interface CategoryRepository extends JpaRepository<Category, UUID> {
     boolean existsByNameIgnoreCase(String name);
+    boolean existsByNameIgnoreCaseAndIdNot(String name, UUID id);
 }
